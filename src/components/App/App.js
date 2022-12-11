820b2e6ba76ec50c37e589eaf5714b5aa1bea1ab
Fatality6 <83357001+Fatality6@users.noreply.github.com>
2022-12-11 12:51:57 +0300

    authWithoutDocker

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
+import useToken from './useToken';
+
+/* function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+} */
+
+
 
 function App() {
+
+  const { token, setToken } = useToken();
+
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
     return (
       <div className="wrapper">
-        <h1>Application</h1>
+        <h1>Trello</h1>
         <BrowserRouter>
-          <Switch>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/preferences">
-              <Preferences />
-            </Route>
-          </Switch>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/preferences' element={<Preferences />} />
+        </Routes>
         </BrowserRouter>
       </div>
     );
